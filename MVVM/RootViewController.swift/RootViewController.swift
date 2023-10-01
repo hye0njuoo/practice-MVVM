8ed3e0c29c849b6cca846a88fd8ddf6e26637773
hyeonju <shj040201@naver.com>
2023-10-01 23:59:45 +0900

    no-storyBoard setting #1

---
+++ MVVM/RootViewController.swift/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by 성현주 on 10/1/23.
+//
+
+import UIKit
+import RxSwift
+import SnapKit
+
+class RootViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        configureUI()
+    }
+    
+    func configureUI() {
+        
+    }
+
+
+}
+
